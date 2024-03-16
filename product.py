5ba204a4c8e360f9965b61a3f12a6506881461dc
Julya Shapaeva <shapaeva03@mail.ru>
2024-03-16 17:02:29 +0300

    Complete the 2d task in product.py

--- product.py
+++ product.py
@@ -6,5 +6,11 @@ class Product:
     cost: float
     count_in_stock: int
 
-    def __init__(self):
-        pass
+    count_products = 0
+
+    def __init__(self, name, description, cost, count_in_stock):
+        self.name = name
+        self.description = description
+        self.cost = cost
+        self.count_in_stock = count_in_stock
+        count_products += 1
