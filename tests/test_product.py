fc95053a2232d6bf271898e5a9a5337470704037
Julya Shapaeva <shapaeva03@mail.ru>
2024-03-16 17:29:36 +0300

    Add tests to test_product.py

---
+++ tests/test_product.py
@@ -0,0 +1,13 @@
+import pytest
+from src.product import Product
+
+@pytest.fixture
+def product_exmpl():
+    return Product("Яблоко", "Полезный фрукт", 30.5, 53)
+
+def test_init(product_exmpl):
+    assert product_exmpl.name == "Яблоко"
+    assert product_exmpl.description == "Полезный фрукт"
+    assert product_exmpl.cost == 30.5
+    assert product_exmpl.count_in_stock == 53
+    assert Product.count_products == 1
