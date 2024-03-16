d8a65474b8d83b337c77ce54c8c86fa072814dc1
Julya Shapaeva <shapaeva03@mail.ru>
2024-03-16 18:04:59 +0300

    Add docstrings to test_product.py

--- tests/test_product.py
+++ tests/test_product.py
@@ -3,9 +3,13 @@ from src.product import Product
 
 @pytest.fixture
 def product_exmpl():
+    """Тестовый экземпляр класса Product"""
+
     return Product("Яблоко", "Полезный фрукт", 30.5, 53)
 
 def test_init(product_exmpl):
+    """Тест конструктора класса Product"""
+
     assert product_exmpl.name == "Яблоко"
     assert product_exmpl.description == "Полезный фрукт"
     assert product_exmpl.cost == 30.5
