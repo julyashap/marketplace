49486f6de9c862bdf5d2ad50af8b5238f3dbff8b
Julya Shapaeva <shapaeva03@mail.ru>
2024-03-17 17:12:42 +0300

    Change test_init()

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,17 +1,20 @@
 import pytest
 from src.category import Category
+from src.product import Product
 
 @pytest.fixture
 def category_exmpl():
     """Тестовый экземпляр класса Category"""
 
-    return Category("Еда", "Для утоления голода", ["Яблоко", "Шоколад", "Пельмени"])
+    return Category("Еда", "Для утоления голода", [Product("Яблоко", "Полезный фрукт", 30.5, 53),
+                                                   Product("Пельмени", "Вкусный и сытный обед", 214.99, 152)])
 
 def test_init(category_exmpl):
     """Тест конструктора класса Category"""
 
     assert category_exmpl.name == "Еда"
     assert category_exmpl.description == "Для утоления голода"
-    assert category_exmpl.goods == ["Яблоко", "Шоколад", "Пельмени"]
+    assert category_exmpl.goods == [Product("Яблоко", "Полезный фрукт", 30.5, 53),
+                                    Product("Пельмени", "Вкусный и сытный обед", 214.99, 152)]
     assert Category.count_categories == 1
-    assert Category.count_unique_products == 3
+    assert Category.count_unique_products == 2
