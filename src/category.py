b81fe7cfe15ff0453e101a540c4f7803471eaa4b
Julya Shapaeva <shapaeva03@mail.ru>
2024-03-17 17:13:10 +0300

    Add gettter and setter for atribute __goods

--- src/category.py
+++ src/category.py
@@ -7,7 +7,15 @@ class Category:
     def __init__(self, name: str, description: str, goods: list):
         self.name = name
         self.description = description
-        self.goods = goods
+        self.__goods = goods
 
         Category.count_categories += 1
         Category.count_unique_products = len(set(self.goods))
+
+    @property
+    def get_goods(self):
+        return self.__goods
+
+    @get_goods.setter
+    def set_goods(self, product):
+        self.__goods.append(product)
