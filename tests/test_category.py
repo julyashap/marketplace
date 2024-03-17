b122088a659f7779674c705b4f054de38bf0b2a7
Julya Shapaeva <shapaeva03@mail.ru>
2024-03-17 18:56:37 +0300

    Update and add new tests to test_category.py

--- tests/test_category.py
+++ tests/test_category.py
@@ -14,7 +14,20 @@ def test_init(category_exmpl):
 
     assert category_exmpl.name == "Еда"
     assert category_exmpl.description == "Для утоления голода"
-    assert category_exmpl.goods == [Product("Яблоко", "Полезный фрукт", 30.5, 53),
-                                    Product("Пельмени", "Вкусный и сытный обед", 214.99, 152)]
+
     assert Category.count_categories == 1
     assert Category.count_unique_products == 2
+
+
+def test_get_goods(category_exmpl):
+    """Тест метода get_goods()"""
+
+    assert category_exmpl.get_goods == "Яблоко, 30.5 руб. Остаток: 53 шт.\nПельмени, 214.99 руб. Остаток: 152 шт.\n"
+
+
+def test_set_goods(category_exmpl):
+    """Тест метода set_goods()"""
+
+    category_exmpl.set_goods = Product("Мороженое", "Лакомство для всех", 54.99, 36)
+    assert category_exmpl.get_goods == "Яблоко, 30.5 руб. Остаток: 53 шт.\nПельмени, 214.99 руб. Остаток: 152 шт.\n" \
+                                       "Мороженое, 54.99 руб. Остаток: 36 шт.\n"
