15240a9e43ac56d3787ba2ac5899cd2170669ee3
Julya Shapaeva <shapaeva03@mail.ru>
2024-03-21 13:10:45 +0300

    Change get_cost on __cost in __str__() and __add__()

--- src/product.py
+++ src/product.py
@@ -45,10 +45,10 @@ class Product:
     def __str__(self):
         """Возвращает объект класса Product в формате: Продукт, 80 руб. Остаток: 15 шт."""
 
-        return f"{self.name.title()}, {self.get_cost} руб. Остаток: {self.count_in_stock} шт."
+        return f"{self.name.title()}, {self.__cost} руб. Остаток: {self.count_in_stock} шт."
 
     def __add__(self, other):
         """Возвращает сложение двух объектов класса Product в виде сложения их стоимости,
         умноженной на количество на складе"""
 
-        return self.get_cost * self.count_in_stock + other.get_cost * other.count_in_stock
+        return self.__cost * self.count_in_stock + other.__cost * other.count_in_stock
